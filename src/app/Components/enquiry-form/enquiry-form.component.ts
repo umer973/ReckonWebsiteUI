3904282a8390f3dd8da014b649e0492b6d2583d2
umer ali <sheikhumer973@gmail.com>
2021-03-27 22:55:03 -0700

    Implemented validation in Form Controls

--- src/app/Components/enquiry-form/enquiry-form.component.ts
+++ src/app/Components/enquiry-form/enquiry-form.component.ts
@@ -1,27 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'app-form',
   templateUrl: './enquiry-form.component.html',
-  
+
 })
 export class EnquiryFormComponent implements OnInit {
 
-  constructor() { }
+  enquiryForm: FormGroup;
+  isSubmitted: false;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.createFormControls();
+
   }
-  HelpForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    emailAddress: new FormControl(''),
-    MobileNumber: new FormControl(''),
-    description: new FormControl(''),
-  });
+
+  createFormControls() {
+
+    this.enquiryForm = this.formBuilder.group({
+      FirstName: ['', Validators.required],
+      LastName: ['', Validators.required],
+      EmailAddress: ['', Validators.required],
+      MobileNumber: ['', Validators.required],
+      Description: ['', Validators.required]
+
+    });
+  }
+
+  get f() { return this.enquiryForm.controls }
 
   onSubmit() {
     // TODO: Use EventEmitter with form value
-    console.warn(this.HelpForm.value);
+    if (this.enquiryForm.valid) {
+      console.warn(this.enquiryForm.value);
+      alert('Enquiry Submitted')
+      this.enquiryForm.reset();
+
+    }
   }
 }
