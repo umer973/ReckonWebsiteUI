ba1991b92dae53a1bea972d42a9211a5a7c90812
umer973 <sheikhumer973@gmail.com>
2021-04-02 13:41:46 +0530

    Date(02-04-21), menu modification and slider text modifications

--- src/app/Services/common.service.ts
+++ src/app/Services/common.service.ts
@@ -16,38 +16,41 @@ export class CommonService {
       {
         "MenuID": 2, "MenuName": "Product", "path": "",
         "Pages": [
-          { "path": "page2", "PageName": "Automobiles Parts & Spares",
+          { "path": "page2", "PageName": "Business Application",
           "Pages": [
-              { "path": "page2", "SubPageName": "sub menu Lorem ipsum dolor sit amet" },
-              { "path": "page2", "SubPageName": "sub menu" },
-              { "path": "page2", "SubPageName": "sub menu" },
-              { "path": "page2", "SubPageName": "sub menu" },
-              { "path": "page2", "SubPageName": "sub menu" }
+              { "path": "page2", "SubPageName": "Reckon-B2C" },
+              { "path": "page2", "SubPageName": "Reckon-B2B" },
+              { "path": "page2", "SubPageName": "Reckon-Orders" },
+              { "path": "page2", "SubPageName": "Reckon-Mreport" }
             ]
           },
-          { "path": "page2", "PageName": "Chemist Shop" ,
+          { "path": "page2", "PageName": "Verticals" ,
           "Pages": [
-              { "path": "page2", "SubPageName": "sub menu" },
-              { "path": "page2", "SubPageName": "sub menu" },
-              { "path": "page2", "SubPageName": "sub menu" },
-              { "path": "page2", "SubPageName": "sub menu" },
-              { "path": "page2", "SubPageName": "sub menu" },
-              { "path": "page2", "SubPageName": "sub menu" },
-              { "path": "page2", "SubPageName": "sub menu" }
+              { "path": "page2", "SubPageName": "Automobiles Parts & Spares" },
+              { "path": "page2", "SubPageName": "Bakery & Confectionery" },
+              { "path": "page2", "SubPageName": "Books & Stationery" },
+              { "path": "page2", "SubPageName": "Ceramics Tiles & Sanitary" },
+              { "path": "page2", "SubPageName": "Chemist Shop" },
+              { "path": "page2", "SubPageName": "Cosmetics & Personal Care" },
+              { "path": "page2", "SubPageName": "Department & Grocery (POS)" },
+              { "path": "page2", "SubPageName": "Dry Fruits & Spices" },
+              { "path": "page2", "SubPageName": "Eyewear Stores" },
+              { "path": "page2", "SubPageName": "Footwear Showroom" },
+              { "path": "page2", "SubPageName": "Furniture & Home Decor" },
+              { "path": "page2", "SubPageName": "FMCG Distribution" },
+              { "path": "page2", "SubPageName": "Garments & Apparel Showroom" }
             ]
           },
-          { "path": "page2", "PageName": "FMCG Distribution",
+          { "path": "page2", "PageName": "ERP Solutions",
           "Pages": [
-            { "path": "page2", "SubPageName": "sub menu" },
-            { "path": "page2", "SubPageName": "sub menu" },
-            { "path": "page2", "SubPageName": "sub menu" },
-            { "path": "page2", "SubPageName": "sub menu" },
-            { "path": "page2", "SubPageName": "sub menu" },
-            { "path": "page2", "SubPageName": "sub menu" },
-            { "path": "page2", "SubPageName": "sub menu" }
+            { "path": "page2", "SubPageName": "Specialized Supermarket" },
+            { "path": "page2", "SubPageName": "Specialized Auto Parts" },
+            { "path": "page2", "SubPageName": "Specialized Pharmacy" },
+            { "path": "page2", "SubPageName": "Multi Location–Branch Synchronization" },
+            { "path": "page2", "SubPageName": "Solution" }
            ]   
           },
-          { "path": "page2", "PageName": "Department & Grocery (POS)",
+          /*{ "path": "page2", "PageName": "Department & Grocery (POS)",
           "Pages": [
             { "path": "page2", "SubPageName": "sub menu" },
             { "path": "page2", "SubPageName": "sub menu" },
@@ -57,7 +60,7 @@ export class CommonService {
             { "path": "page2", "SubPageName": "sub menu" },
             { "path": "page2", "SubPageName": "sub menu" }
            ]
-          }
+          }*/
         ]
       },
       { "MenuID": 3, "MenuName": "Customer", "path": "", },
